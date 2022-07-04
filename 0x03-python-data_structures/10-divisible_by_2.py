5ab967af73e2b785b19706c899f7d63f95c8c515
Han-nahh <hannatesfaye11@gmail.com>
2022-07-04 14:10:06 +0000

    jul4

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 def divisible_by_2(my_list=[]):
-    for i in range (len(my_list)):
-        if my_list[i]%2==0:
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
             print("{:d} divisible by 2".format(my_list[i]))
         else:
             print("{:d} is not divisible by 2".format(my_list[i]))
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def max_integer(my_list=[]):
     if len(my_list) > 0:
         for i in range(len(my_list)):
@@ -11,4 +10,4 @@ def max_integer(my_list=[]):
 
             return my_list[i]
     else:
-        return None;
+        return None
