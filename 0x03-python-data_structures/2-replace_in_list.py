fd30e387f55bdb60733416f62200b911830c2442
Han-nahh <hannatesfaye11@gmail.com>
2022-07-04 13:42:28 +0000

    jul4

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-def replace_in_list(my_list,idx,element):
-    #idx = int(input("Input to which index you need this imported"));
-    #element=int(input("Enter the new number"));
-    if (idx < 0 ):
-        print("None");
-    elif(idx>=len(my_list)):
-        return my_list;
-
+def replace_in_list(my_list, idx, element):
+    if (idx < 0):
+        return None
+    elif(idx >= len(my_list)):
+        return my_list
     else:
-        my_list[idx]=element;
+        my_list[idx] = element
         return my_list
