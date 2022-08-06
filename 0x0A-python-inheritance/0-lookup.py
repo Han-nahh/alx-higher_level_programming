aa12473910eb45713601a849359178b75a1280f1
Han-nahh <hannatesfaye11@gmail.com>
2022-08-07 01:45:40 +0300

    update

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-"""lookup"""
 
-def lookup(obj):
-    """function that returns the list of available attributes and methods of an object"""
+
+
+def inherits_from(obj, a_class):
+    """A function that returns True if the object is
+    an instance of a class that inherited (directly or indirectly)
+    from the specified class;
+    otherwise False.
+    """
     return dir(obj)
